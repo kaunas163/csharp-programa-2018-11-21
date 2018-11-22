ba9aaa050e61d8de4885482785407491e159f95d
Ieva <ieva.jaksaityte@gmail.com>
2018-11-22 09:38:55 +0200

    Add project files.

---
+++ Parduotuve/Gerimas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parduotuve
+{
+    class Gerimas
+    {
+        public string Gamintojas { get; private set; }
+        public string Pavadinimas { get; private set; }
+        public double Laipsniai { get; private set; }
+        public double TiekimoKaina { get; private set; }
+        public double Turis { get; private set; }
+
+        public Gerimas(string eilute)
+        {
+            var isskaidyta = eilute.Split(';');
+
+            Gamintojas = isskaidyta[0];
+            Pavadinimas = isskaidyta[1];
+            Laipsniai = Convert.ToDouble(isskaidyta[2]);
+            TiekimoKaina = Convert.ToDouble(isskaidyta[3]);
+            Turis = Convert.ToDouble(isskaidyta[4]);
+        }
+
+        public void Isvedimas()
+        {
+            Console.WriteLine("| {0,-15} | {1,-15} | {2,-5} | {3,-5} | {4,-5} |",
+                Gamintojas, Pavadinimas, Laipsniai, TiekimoKaina,
+                Turis);
+        }
+    }
+}
---
+++ Parduotuve/Parduotuve.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parduotuve
+{
+    class Parduotuve
+    {
+        public string Pavadinimas { get; private set; }
+        public string Adresas { get; private set; }
+        public string Bosas { get; private set; }
+        public List<Gerimas> Gerimai { get; private set; }
+
+        public Parduotuve()
+        {
+            Console.Write("Iveskite pavadinima: ");
+            Pavadinimas = Console.ReadLine();
+
+            Console.Write("Iveskite adresa: ");
+            Adresas = Console.ReadLine();
+
+            Console.Write("Iveskite bosa: ");
+            Bosas = Console.ReadLine();
+
+            Console.Write("Iveskite kur yra gerimu failas: ");
+            var failas = Console.ReadLine();
+
+            var tekstas = File.ReadAllLines(failas);
+            Gerimai = new List<Gerimas>();
+
+            for (int i = 1; i < tekstas.Length; i++)
+            {
+                var gerimas = new Gerimas(tekstas[i]);
+                Gerimai.Add(gerimas);
+            }
+        }
+
+        public void Isvedimas()
+        {
+            Console.WriteLine("Parduotuve {0} randasi adresu {1}, jos bosas {2}",
+                Pavadinimas, Adresas, Bosas);
+            Console.WriteLine("Turimi gerimai");
+            Console.WriteLine("| {0,-15} | {1,-15} | {2,-5} | {3,-5} | {4,-5} |",
+                "Gamintojas", "Pavadinimas", "Laip.", "Kaina", "Turis");
+            foreach (var gerimas in Gerimai)
+            {
+                gerimas.Isvedimas();
+            }
+            Console.WriteLine();
+        }
+    }
+}
